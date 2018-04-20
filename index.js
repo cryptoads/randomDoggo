5873528c2b65c11f8667ef599c32f0f8e23c425e
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-19 20:16:01 -0400

    fixing button text

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 $(function(){
 
+$.get('https://dog.ceo/api/breeds/list');
+
 $('button').on('click', function() {
     $(this).html('Generating Doggo....');
      $.get('https://dog.ceo/api/breeds/image/random', function (data) {
         var image = '<img src='+data.message+'>';
         $('body').append(image);
+        $('button').html('Generate Doggo');
      }); 
-    $(this).html('Generate Doggo');
+
 })
 });
