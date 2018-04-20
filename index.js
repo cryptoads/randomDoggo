265b6b66e3649433cbb522256a666ffdd2c7545e
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-19 20:33:06 -0400

    doggo dropdown

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $(function(){
 
-$.get('https://dog.ceo/api/breeds/list');
+$.get('https://dog.ceo/api/breeds/list', function (data) {
+    data.message.forEach( function(breed) {
+        $('select').append('<option value="'+breed+'">'+breed+'</option>')
+    });
+});
+
+$('select').change(function () {
+    var breeds = $('select').val();
+    $.get('https://dog.ceo/api/breed/'+breeds+'/images/random', function(data){
+        var image = '<img src='+data.message+'>';
+        $('body').append(image);
+    })
+})
 
 $('button').on('click', function() {
     $(this).html('Generating Doggo....');
